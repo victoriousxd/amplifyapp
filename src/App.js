cf6dfba6a9659c9639b05e04865f31ee36e336b5
Victoria Hong <victoriousxd@gmail.com>
2021-03-16 17:50:07 -0400

    added a chicken

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import chicken from './assets/chicken_thumbs_up.gif'
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={chicken} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload 
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <bob/>
           Learn React
         </a>
       </header>
@@ -23,5 +28,3 @@ function App() {
 }
 
 export default App;
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
